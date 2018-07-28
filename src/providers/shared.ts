67e340b3a204149ce3e6d85ee4fcbfbe515a0ea5
Bharathsb <bharath.infy1993@gmail.com>
2018-07-28 23:02:39 +0530

    [image reverted]

--- src/providers/shared.ts
+++ src/providers/shared.ts
@@ -109,5 +109,6 @@ export class Shared {
             var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
             document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         }
+        window.postMessage({ type: "CLEAR_COOKIES_DOCUMENT" }, "*");
     }
 }
